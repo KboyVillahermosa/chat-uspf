0faa057eb8b87bb73d9ce70dc5d26fc4ac48dde5
KboyVillahermosa <villahermosafrancisco6@gmail.com>
2024-05-17 21:34:42 +0800

    update

--- chat.php
+++ chat.php
@@ -1,47 +1,9 @@
-<?php
-session_start();
-include_once "php/config.php";
-
-// Check if user is logged in
-if(!isset($_SESSION['unique_id'])){
-  header("location: login.php");
-  exit;
-}
-
-// Fetch group chat details
-if (isset($_GET['group_id'])) {
-    $group_id = $_GET['group_id'];
-    // Fetch group chat name
-    $sql_group = "SELECT * FROM groups WHERE group_id = $group_id";
-    $result_group = mysqli_query($conn, $sql_group);
-    if ($result_group && mysqli_num_rows($result_group) > 0) {
-        $group = mysqli_fetch_assoc($result_group);
-        $group_name = $group['group_name'];
-    } else {
-        // Handle error
-        echo "Group not found";
-        exit;
-    }
-
-    // Fetch group members
-    $sql_members = "SELECT users.* FROM users 
-                    INNER JOIN group_members ON users.unique_id = group_members.user_id
-                    WHERE group_members.group_id = $group_id";
-    $result_members = mysqli_query($conn, $sql_members);
-    $members = [];
-    if ($result_members && mysqli_num_rows($result_members) > 0) {
-        while ($row = mysqli_fetch_assoc($result_members)) {
-            $members[] = $row;
-        }
-    } else {
-        // Handle no members found
-        echo "No members found";
-    }
-} else {
-    // Handle no group_id provided
-    echo "Group ID not provided";
-    exit;
-}
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
 ?>
 <?php include_once "header.php"; ?>
 <body>
